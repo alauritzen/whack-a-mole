70207e5df35805c38eb0e723b4932b5e6e99c537
Alan <alaurit@gmail.com>
2016-04-13 08:34:49 -0500

    added images, fixed scoring, intervals; need to eliminate dupes and add sound

--- public/js/whack_js.js
+++ public/js/whack_js.js
@@ -1,9 +1,10 @@
 (function() {
 
 var moleHill = $(".mole_hill");
+var score = 0;
 
 // create grid layout
-moleHill.each(function (index, tile) {
+moleHill.each(function (index, square) {
     if ((index) % 3 == 0)  {
         $(this).css("clear", "left");
     };
@@ -19,32 +20,76 @@ function chooseMoleHill (min, max) {
 // function to flash moles
 // include timer function
 function moleAttack() {
-    var timer = 10;
     // begin timer function
+    var timer = 10;
     var intervalId = setInterval(function () {
         if (timer > 0) {
             timer--;
             $("#seconds").text(timer);
         } else {
             clearInterval(moleInterval);
+            animation();
         }
     }, 1000);
-    // begin mole interval function
-    var interval = 1200;
-    var moleInterval = setInterval(function() {
-        var index=chooseMoleHill();
-        console.log(index);
-        moleHill.eq(index).css("background-color", "red");
-        // add animation here
+    // function whack is what happens when you whack a mole
+    var whack = function () {
+        $(this).animate({
+            opacity: "0.0"
+        },moleSpeed);
+        if (timer > 0) {
+            score++;                
+        }
+        $("#score").text(score);
+        $(this).off("mouseover", whack);
+    };
 
+    var noWhack = function(square) {
+        console.log("noWhack ran");
+        $(square).animate({
+            opacity: "0.0"
+        },1);
+        $(square).off("mouseover", whack);
+    };
+
+    // begin mole interval function
+    var interval = 1300;
+    var moleSpeed = 600;
+    var moleInterval = setInterval(function() {
+        var index = chooseMoleHill();
+        var square = moleHill.eq(index);
+        console.log(square);
+        square.animate({
+            opacity: "1.0"
+        },moleSpeed);
+        square.on("mouseover", whack);
+        var moleLeave = setTimeout(function() {
+            noWhack(square);
+        }, moleSpeed);
     }, interval);
+
+
+
+/*
+        square.animate({
+            opacity: "0.0"
+        }, interval);
+        square.off("mouseover", whack);
+        */
+        
+        
 };
 
 
-// moleHill.eq(1).css("background-color", "red");
+function animation() {
+    $("#gollum").animate({
+        left: "-100",
+        bottom: "-220"
+    }, 200);
+}
 
 
 moleAttack();
+
 
 
 
